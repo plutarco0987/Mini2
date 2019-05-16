1e63ef4ba0566eb551d3942be7c948e3134b5cd9
Plutarco <plutarco0987@hotmail.com>
2019-05-15 18:14:21 -0700

    Juntado todo 1.0

--- app/src/main/java/c/plutarco/geolocalizacin/Buscar.java
+++ app/src/main/java/c/plutarco/geolocalizacin/Buscar.java
@@ -1,6 +1,18 @@
 package c.plutarco.geolocalizacin;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +20,8 @@ import com.google.firebase.database.DatabaseError;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import com.google.firebase.database.ValueEventListener;
 
 import com.google.firebase.FirebaseApp;
@@ -19,9 +33,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 public class Buscar extends AppCompatActivity {
 
@@ -32,6 +48,10 @@ public class Buscar extends AppCompatActivity {
     DatabaseReference databaseReference;
     EditText nombre;
     ListView listadatos;
+    double latitud;
+    double longitud;
+    TextView latitudtexto;
+    TextView longitudtexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +59,14 @@ public class Buscar extends AppCompatActivity {
 
         nombre= findViewById(R.id.nombre);
         listadatos= findViewById(R.id.lista);
+        latitudtexto=(TextView)findViewById(R.id.latitudtexto);
+        longitudtexto=(TextView)findViewById(R.id.longitudtexto);
         iniciarfirebase();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+            locationStart();
+        }
 
         listadatos();
     }
@@ -69,6 +96,99 @@ public class Buscar extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
          firebaseDatabase= FirebaseDatabase.getInstance();
         databaseReference= firebaseDatabase.getReference();
+    }
+
+    /*
+    * Metodos para obtener la geolocalización y se almacenan en los valores al principio
+    * */
+    private void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setMainActivity(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+            return;
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);;
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationStart();
+                return;
+            }
+        }
+    }
+    public void setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address DirCalle = list.get(0);
+                    //mensaje2.setText("Mi direccion es: \n"
+                     //       + DirCalle.getAddressLine(0));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public class Localizacion implements LocationListener {
+        Buscar mainActivity;
+        public Buscar getMainActivity() {
+            return mainActivity;
+        }
+        public void setMainActivity(Buscar mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+        @Override
+        public void onLocationChanged(Location loc) {
+            // Este metodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
+            // debido a la deteccion de un cambio de ubicacion
+            loc.getLatitude();
+            loc.getLongitude();
+            String Text = "Mi ubicacion actual es: " + "\n Lat = "
+                    + loc.getLatitude() + "\n Long = " + loc.getLongitude();
+            //mensaje1.setText(Text);
+            longitud=loc.getLongitude();
+            latitud=loc.getLatitude();
+            latitudtexto.setText(String.valueOf(latitud));
+            longitudtexto.setText(String.valueOf(longitud));
+            this.mainActivity.setLocation(loc);
+        }
+        @Override
+        public void onProviderDisabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es desactivado
+            //mensaje1.setText("GPS Desactivado");
+        }
+        @Override
+        public void onProviderEnabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es activado
+            //mensaje1.setText("GPS Activado");
+        }
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("debug", "LocationProvider.AVAILABLE");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                    break;
+            }
+        }
     }
 }
 
--- app/src/main/java/c/plutarco/geolocalizacin/PantallaInicio.java
+++ app/src/main/java/c/plutarco/geolocalizacin/PantallaInicio.java
@@ -1,7 +1,10 @@
 package c.plutarco.geolocalizacin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PantallaInicio extends AppCompatActivity {
 
@@ -9,9 +12,31 @@ public class PantallaInicio extends AppCompatActivity {
      * Pagina principal donde el usuario decidira o unirse a un grupo cercano o crear un grupo para usuarios sercanos
      * @param savedInstanceState
      */
+    Button crear;
+    Button cliente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_inicio);
+
+        crear= (Button) findViewById(R.id.botoncreargrupo);
+        cliente= (Button) findViewById(R.id.botonbuscargrupo);
+
+        crear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(PantallaInicio.this,FormCrear.class);
+                startActivity(i);
+            }
+        });
+        cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(PantallaInicio.this,Buscar.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
